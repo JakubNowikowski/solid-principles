04cf47b6e813fafee59a658df97579f984e7f228
Jakub Nowikowski <kubanowikowski@gmail.com>
2020-01-28 14:38:02 +0100

    SRP principle added

---
+++ solid-principles/UserServiceImplementingSRP.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid_principles
+{
+    class UserServiceImplementingSRP
+    {
+        private FileLogger fileLogger = new FileLogger();
+        public void Add()
+        {
+            try
+            {
+                // Database code
+            }
+            catch (Exception ex)
+            {
+                fileLogger.Handle(ex.ToString());
+            }
+        }
+    }
+}
---
+++ solid-principles/UserServiceWithoutSRP.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid_principles
+{
+    class UserServiceWithoutSRP
+    {
+        public void Register(string email, string password)
+        {
+            try
+            {
+                // Database code
+            }
+            catch (Exception ex)
+            {
+                System.IO.File.WriteAllText(@"c:\Error.txt", ex.ToString());
+            }
+        }
+    }
+}
